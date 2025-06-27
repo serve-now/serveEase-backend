3ed7c3bd3987913873d2f5d7b6a9e5b32ef18507
wjdwwidz <wjdwwidz@gmail.com>
2025-06-27 19:29:38 +0900

    Feat : Add indByTableNumber, findByStatus

--- src/main/java/com/servease/demo/repository/RestaurantTableRepository.java
+++ src/main/java/com/servease/demo/repository/RestaurantTableRepository.java
@@ -1,10 +1,18 @@
 package com.servease.demo.repository;
 
 import com.servease.demo.model.entity.RestaurantTable;
+import com.servease.demo.model.enums.RestaurantTableStatus;
+import com.servease.demo.service.RestaurantTableService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
 
+    Optional<RestaurantTable> findByTableNumber(Integer tableNumber);
+
+    List<RestaurantTable> findByStatus(RestaurantTableStatus status);
 }
