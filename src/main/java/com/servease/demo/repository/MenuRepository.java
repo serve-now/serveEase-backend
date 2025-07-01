ba77343b9279e3bddfb3ac96b4c13bad77f742b0
wjdwwidz <wjdwwidz@gmail.com>
2025-07-01 16:09:02 +0900

    Feat : findByName, findByAvailableTrue method

--- src/main/java/com/servease/demo/repository/MenuRepository.java
+++ src/main/java/com/servease/demo/repository/MenuRepository.java
@@ -4,7 +4,14 @@ import com.servease.demo.model.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu,Long> {
+
+    Optional<Menu> findByName(String name);
+
+    List<Menu> findByIsAvailableTrue();
 
 }
