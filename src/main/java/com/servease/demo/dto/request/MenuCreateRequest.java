019ab2cb5d87dcd8fa0e3dc76578d94e37fc03f4
wjdwwidz <wjdwwidz@gmail.com>
2025-07-24 19:27:54 +0900

    Feat : Set isAvailable field default=true

--- src/main/java/com/servease/demo/dto/request/MenuCreateRequest.java
+++ src/main/java/com/servease/demo/dto/request/MenuCreateRequest.java
@@ -11,5 +11,5 @@ public class MenuCreateRequest {
     private String name;
     private int price;
     private String category;
-    private boolean isAvailable;
+    private boolean isAvailable = true;
 }
