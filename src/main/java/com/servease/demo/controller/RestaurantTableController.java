6b189e9565db7981f39f8e03c589bba02389e60e
wjdwwidz <wjdwwidz@gmail.com>
2025-08-08 17:22:57 +0900

    Feat : Add getAllTables, createTable

--- src/main/java/com/servease/demo/controller/RestaurantTableController.java
+++ src/main/java/com/servease/demo/controller/RestaurantTableController.java
@@ -1,10 +1,14 @@
 package com.servease.demo.controller;
 
-import com.servease.demo.model.entity.RestaurantTable;
+import com.servease.demo.dto.request.RestaurantTableCreateRequest;
+import com.servease.demo.dto.response.RestaurantTableResponse;
 import com.servease.demo.service.RestaurantTableService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tables")
@@ -12,13 +16,22 @@ public class RestaurantTableController {
 
     private final RestaurantTableService restaurantTableService;
 
+    @Autowired
     public RestaurantTableController(RestaurantTableService restaurantTableService) {
         this.restaurantTableService = restaurantTableService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<RestaurantTableResponse>> getAllTables(){
+        List<RestaurantTableResponse> tableResponses = restaurantTableService.getAllTables();
+        return ResponseEntity.ok(tableResponses);
+    }
+
+
     @PostMapping
-    public ResponseEntity<RestaurantTable> createTable(@RequestParam Integer tableNumber) {
-        RestaurantTable newTable = restaurantTableService.createTable(tableNumber);
+    public ResponseEntity<RestaurantTableResponse> createTable(@RequestBody RestaurantTableCreateRequest request) {
+        RestaurantTableResponse newTable = restaurantTableService.createTable(request.getRestaurantTableNumber());
         return ResponseEntity.status(HttpStatus.CREATED).body(newTable);
     }
+
 }
