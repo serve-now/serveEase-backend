a4c414e18df3220aae0d2f9949fc8d03ccfa35ad
wjdwwidz <wjdwwidz@gmail.com>
2025-08-08 16:16:48 +0900

    Feat : Add RestaurantTableResponse

---
+++ src/main/java/com/servease/demo/dto/response/RestaurantTableResponse.java
@@ -0,0 +1,27 @@
+package com.servease.demo.dto.response;
+
+import com.servease.demo.model.entity.RestaurantTable;
+import com.servease.demo.model.enums.RestaurantTableStatus;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+public class RestaurantTableResponse {
+    private Long id;
+    private Integer tableNumber;
+    private RestaurantTableStatus status;
+
+    public static RestaurantTableResponse fromEntity(RestaurantTable restaurantTable){
+        if(restaurantTable == null) {
+            return null;
+        }
+
+        return RestaurantTableResponse.builder()
+                .id(restaurantTable.getId())
+                .tableNumber(restaurantTable.getTableNumber())
+                .status(restaurantTable.getStatus())
+                .build();
+    }
+
+}
