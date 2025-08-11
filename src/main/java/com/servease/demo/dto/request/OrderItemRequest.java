8225e8df83f27f4bdd30ff2f2c875b13b9ec6d61
wjdwwidz <wjdwwidz@gmail.com>
2025-08-11 16:51:18 +0900

    Feat : Add OrderItemRequest

--- src/main/java/com/servease/demo/dto/request/OrderItemRequest.java
+++ src/main/java/com/servease/demo/dto/request/OrderItemRequest.java
@@ -1,13 +1,22 @@
 package com.servease.demo.dto.request;
 
-import lombok.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@AllArgsConstructor
 public class OrderItemRequest {
+    @NotNull(message = "메뉴 ID는 필수입니다.")
+    private Long menuId;
 
-
+    @Min(value = 1, message = "수량은 최소 1개 이상이어야 합니다.")
+    private int quantity;
 }
