fd1ebee8774b16cbde142bdd7a452f91ed5b58f0
wjdwwidz <wjdwwidz@gmail.com>
2025-08-27 16:39:17 +0900

    Feat : Add findAllByStatusIn

--- src/main/java/com/servease/demo/repository/OrderRepository.java
+++ src/main/java/com/servease/demo/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.servease.demo.repository;
 
 import com.servease.demo.model.entity.Order;
+import com.servease.demo.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     //테이블당 활성 주문은 하나
     Optional<Order> findByRestaurantTableIdAndIsPaidFalse (Long restaurantTableId);
+
+
+    // active : RECEIVED, IN_PROGRESS
+    // inactive : CANCELED
+    List<Order> findAllByStatusIn(Collection<OrderStatus> statuses);
 }
