14680f84214122e19390287db7f1f9bcfd2788df
wjdwwidz <wjdwwidz@gmail.com>
2025-08-29 17:26:47 +0900

    Feat : Add status SERVED

--- src/main/java/com/servease/demo/model/enums/OrderStatus.java
+++ src/main/java/com/servease/demo/model/enums/OrderStatus.java
@@ -4,5 +4,6 @@ package com.servease.demo.model.enums;
 public enum OrderStatus {
     CANCELED,
     RECEIVED,
-    COMPLETED,
+    SERVED, // SERVED : paid 와 무관, 음식이 서빙되었다면 SERVED 로 변경되는 독립적인 상태
+    COMPLETED, // isPaid = true, OrderStatus = SERVED 두 가지의 상태를 만족할 때 COMPLETED 로 변경됨
 }
