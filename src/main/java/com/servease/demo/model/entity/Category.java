d1743bb4b8dab77f1e06cf9c4e56beb98ae7a0a4
wjdwwidz <wjdwwidz@gmail.com>
2025-09-11 23:57:05 +0900

    Feat : Add Category entity

---
+++ src/main/java/com/servease/demo/model/entity/Category.java
@@ -0,0 +1,20 @@
+package com.servease.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+}
