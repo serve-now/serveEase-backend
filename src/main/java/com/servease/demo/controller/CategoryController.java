ab025f07692cc30cfcd43707300697f8d5416827
wjdwwidz <wjdwwidz@gmail.com>
2025-09-12 02:20:55 +0900

    Feat : Add Category Controller

---
+++ src/main/java/com/servease/demo/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package com.servease.demo.controller;
+
+import com.servease.demo.dto.request.CategoryRequest;
+import com.servease.demo.dto.response.CategoryResponse;
+import com.servease.demo.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> createCategory(@RequestBody CategoryRequest request) {
+        CategoryResponse categoryResponse = categoryService.createCategory(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categoryResponses = categoryService.getAllCategories();
+        return ResponseEntity.ok(categoryResponses);
+    }
+
+}
