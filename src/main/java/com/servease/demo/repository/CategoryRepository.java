d8eee0ac0cbcafa82f6f209bbd3eb719c615f23d
wjdwwidz <wjdwwidz@gmail.com>
2025-09-12 01:56:00 +0900

    Feat : Add CategoryRepository

---
+++ src/main/java/com/servease/demo/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.servease.demo.repository;
+
+import com.servease.demo.model.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
