b73357db70ef5e1086fea1fbe5b4d16596231fd6
wjdwwidz <wjdwwidz@gmail.com>
2025-09-20 00:43:56 +0900

    Refactor

--- src/main/java/com/servease/demo/config/SecurityConfig.java
+++ src/main/java/com/servease/demo/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.servease.demo.Config;
+package com.servease.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
