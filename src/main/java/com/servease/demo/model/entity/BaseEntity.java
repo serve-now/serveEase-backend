e392e88e1baa836612039ae00d043e9fb8f82218
wjdwwidz <wjdwwidz@gmail.com>
2025-09-20 01:04:22 +0900

    Feat: Add BaseEntity

--- src/main/java/com/servease/demo/model/entity/BaseEntity.java
+++ src/main/java/com/servease/demo/model/entity/BaseEntity.java
@@ -3,8 +3,6 @@ package com.servease.demo.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
