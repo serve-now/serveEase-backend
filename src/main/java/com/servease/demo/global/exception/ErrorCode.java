a5734f7c4c651b7903e6158660d00ff84bf9e06f
wjdwwidz <wjdwwidz@gmail.com>
2025-09-22 22:29:42 +0900

    Feat: Add ErrorCode userNotFound

--- src/main/java/com/servease/demo/global/exception/ErrorCode.java
+++ src/main/java/com/servease/demo/global/exception/ErrorCode.java
@@ -18,6 +18,7 @@ public enum ErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "E004", "주문을 찾을 수 없습니다."),
     TABLE_NOT_FOUND(HttpStatus.NOT_FOUND, "E010", "테이블을 찾을 수 없습니다."),
     ORDER_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "E012", "해당 주문 항목을 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"E016","해당 유저를 찾을 수 없습니다."),
 
     // 409 Conflict
     DUPLICATE_MENU_NAME(HttpStatus.CONFLICT, "E005", "이미 존재하는 메뉴 이름입니다."),
@@ -27,6 +28,7 @@ public enum ErrorCode {
     DUPLICATE_TABLE_NUMBER(HttpStatus.CONFLICT, "E011", "이미 존재하는 테이블 번호입니다."),
     ACTIVE_ORDER_EXISTS(HttpStatus.CONFLICT, "E013", "이미 진행 중인 주문이 테이블에 존재합니다."),
     ORDER_ALREADY_PAID(HttpStatus.CONFLICT, "E014", "이미 결제가 완료된 주문입니다."),
+    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "E015", "이미 사용 중인 아이디입니다."),
 
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E999", "서버 내부 오류가 발생했습니다.");
