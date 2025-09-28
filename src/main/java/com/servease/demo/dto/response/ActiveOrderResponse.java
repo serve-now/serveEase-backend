aecc897cf0a653728d3ddf005bc3ef1988e15ec1
wjdwwidz <wjdwwidz@gmail.com>
2025-09-28 21:20:50 +0900

    Feat: Add ActiveOrderResponse

---
+++ src/main/java/com/servease/demo/dto/response/ActiveOrderResponse.java
@@ -0,0 +1,29 @@
+package com.servease.demo.dto.response;
+
+import com.servease.demo.model.entity.Order;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+public class ActiveOrderResponse {
+    private Long orderId;
+    private Integer totalPrice;
+    private List<OrderItemSummaryResponse> orderItems;
+
+    public static ActiveOrderResponse fromEntity(Order order) {
+        if (order == null) {
+            return null;
+        }
+        return ActiveOrderResponse.builder()
+                .orderId(order.getId())
+                .totalPrice(order.getTotalPrice())
+                .orderItems(order.getOrderItems().stream()
+                        .map(OrderItemSummaryResponse::fromEntity)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+}
