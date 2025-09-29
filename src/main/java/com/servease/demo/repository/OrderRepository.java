6f5d3ff56bae7c427626c991cfb893d8cc017c44
wjdwwidz <wjdwwidz@gmail.com>
2025-09-29 12:43:04 +0900

    Feat: Add findByRestaurantTable_StoreIdAndStatus, findByRestaurantTable_StoreIdAndStatusIn

--- src/main/java/com/servease/demo/repository/OrderRepository.java
+++ src/main/java/com/servease/demo/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.servease.demo.repository;
 
 import com.servease.demo.model.entity.Order;
 import com.servease.demo.model.enums.OrderStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
@@ -12,6 +14,10 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByRestaurantTableId(Long restaurantTableId);
 
     List<Order> findByRestaurantTableIdAndStatusIn(Long restaurantTableId, Collection<OrderStatus> statuses);
+
+    Page<Order> findByRestaurantTable_StoreIdAndStatus(Long storeId, OrderStatus status, Pageable pageable);
+
+    Page<Order> findByRestaurantTable_StoreIdAndStatusIn(Long storeId, List<OrderStatus> statuses, Pageable pageable);
 
     // active : ORDERED, IN_PROGRESS
     // inactive : CANCELED
