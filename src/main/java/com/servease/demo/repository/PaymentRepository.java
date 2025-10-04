97d0a61055370094bd82b8a168a89655c135363a
wjdwwidz <wjdwwidz@gmail.com>
2025-10-05 01:25:15 +0900

    Feat: Add PaymentRepository

---
+++ src/main/java/com/servease/demo/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.servease.demo.repository;
+
+import com.servease.demo.model.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+}
+
