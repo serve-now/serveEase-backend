5c36eb6f4c58d271abc4ac2f90896319e3e2bd9e
wjdwwidz <wjdwwidz@gmail.com>
2025-10-05 01:24:07 +0900

    Feat: Add Payment

---
+++ src/main/java/com/servease/demo/model/entity/Payment.java
@@ -0,0 +1,52 @@
+package com.servease.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+
+@Entity
+@Table(name = "payments",
+        indexes = {
+                @Index(name = "ux_payments_payment_key", columnList = "payment_key", unique = true),
+                @Index(name = "ux_payments_order_id", columnList = "order_id", unique = true),
+                @Index(name = "ix_payments_created_at", columnList = "created_at")
+        }
+)
+@Getter @Builder @NoArgsConstructor @AllArgsConstructor
+public class Payment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="payment_key", nullable=false, length=200, unique=true)
+    private String paymentKey;
+
+    @Column(name="order_id", nullable=false, length=64)
+    private String orderId;
+
+    @Column(name="amount", nullable=false)
+    private Integer amount;
+
+    @Lob @Column(name="raw", columnDefinition="TEXT", nullable=false)
+    private String raw; // TossConfirm 성공 응답 전체
+
+    @Column(name="created_at", nullable=false, updatable=false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist void pre() {
+        if (createdAt == null) createdAt = OffsetDateTime.now();
+    }
+
+    public static Payment from(String paymentKey, String orderId, Integer amount, String rawJson) {
+        return Payment.builder()
+                .paymentKey(paymentKey)
+                .orderId(orderId)
+                .amount(amount)
+                .raw(rawJson)
+                .build();
+    }
+}
