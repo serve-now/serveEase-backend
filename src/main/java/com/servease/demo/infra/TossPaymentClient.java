80d1b616620662a5c5315b3437c6d6c23d9c926a
wjdwwidz <wjdwwidz@gmail.com>
2025-10-13 03:18:25 +0900

    Feat: Create TossPaymentClient and DTO

---
+++ src/main/java/com/servease/demo/infra/TossPaymentClient.java
@@ -0,0 +1,32 @@
+package com.servease.demo.infra;
+
+import com.servease.demo.dto.PaymentResponseDto;
+import com.servease.demo.dto.request.TossConfirmRequest;
+import com.servease.demo.global.exception.BusinessException;
+import com.servease.demo.global.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+public class TossPaymentClient {
+    private final RestTemplate restTemplate;
+    private final String TOSS_URL = "https://api.tosspayments.com/v1/payments/confirm";
+
+    public TossPaymentClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public PaymentResponseDto confirm(TossConfirmRequest request) {
+        ResponseEntity<PaymentResponseDto> res = restTemplate.postForEntity(TOSS_URL, request, PaymentResponseDto.class);
+        if (!res.getStatusCode().is2xxSuccessful()) {
+            log.error("Toss confirm failed. Response: {}", res.getBody());
+            throw new BusinessException(ErrorCode.TOSS_PAYMENT_CONFIRM_FAILED);
+        }
+
+        return res.getBody();
+
+    }
+}
