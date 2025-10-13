c5c7453763a3426da056d47d060b6e2fc1fadbd4
wjdwwidz <wjdwwidz@gmail.com>
2025-10-13 15:54:26 +0900

    Refactor: Apply tossConfirmRequest, PaymentConfirmResponse

--- src/main/java/com/servease/demo/service/PaymentService.java
+++ src/main/java/com/servease/demo/service/PaymentService.java
@@ -1,45 +1,41 @@
 package com.servease.demo.service;
 
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.servease.demo.dto.PaymentResponseDto;
 import com.servease.demo.dto.request.TossConfirmRequest;
 import com.servease.demo.dto.response.PaymentConfirmResponse;
 import com.servease.demo.global.exception.BusinessException;
 import com.servease.demo.global.exception.ErrorCode;
+import com.servease.demo.infra.TossPaymentClient;
 import com.servease.demo.model.entity.Order;
 import com.servease.demo.model.entity.Payment;
 import com.servease.demo.repository.OrderRepository;
 import com.servease.demo.repository.PaymentRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
 
-    private final RestTemplate restTemplate;
+    private final TossPaymentClient tossPaymentClient;
     private final PaymentRepository paymentRepository;
     private final OrderRepository orderRepository; // OrderRepository 주입
     private final OrderService orderService;
-    private final ObjectMapper om = new ObjectMapper();
+    private final PlatformTransactionManager transactionManager;
 
     // 사용자가 결제창에서 인증 끝내고 successUrl로 돌아온 순간에 paymentKey, orderId, amount를 서버에 저장
     // 같은트랜잭션으로 saveAndConfirm 바로 호출
     // 10분 넘기면 EXPIRED/NOT_FOUND_PAYMENT_SESSION으로 실패 (toss에서)
 
-    @Transactional
     public PaymentConfirmResponse confirmAndSave(TossConfirmRequest tossConfirmRequest) {
         //응답에서 orderId 추출,  order 찾기 (completePayment 호출 위함)
         String orderId = tossConfirmRequest.orderId();
+        // TODO: orderService의 getOrderById 사용하는 걸로 개선하기.
         Order order = orderRepository.findByOrderId(orderId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.ORDER_NOT_FOUND, "주문을 찾을 수 없습니다. orderId: " + orderId));
 
@@ -51,41 +47,17 @@ public class PaymentService {
             throw new BusinessException(ErrorCode.ORDER_ALREADY_PAID);
         }
 
-        // 토스 페이먼트에 결제 승인 요청(raw 문자열로 받기)
-        var body = Map.of("paymentKey", tossConfirmRequest.paymentKey(),
-                "orderId", tossConfirmRequest.orderId(),
-                "amount", tossConfirmRequest.amount()
-        );
+        PaymentResponseDto response = tossPaymentClient.confirm(tossConfirmRequest);
 
-        ResponseEntity<String> res;
-        try {
-            res = restTemplate.postForEntity("https://api.tosspayments.com/v1/payments/confirm", body, String.class);
-        } catch (HttpStatusCodeException e) {
-            log.error("Toss confirm failed. Response: {}", e.getResponseBodyAsString());
-            throw new BusinessException(ErrorCode.TOSS_PAYMENT_CONFIRM_FAILED);
-        }
-
-        String rawJson = res.getBody();
-
-        // JSON 트리로 파싱 (중간 DTO 없이 그냥 받음)
-        JsonNode root;
-        try {
-            root = om.readTree(rawJson);
-        } catch (Exception e) {
-            log.error("Failed to parse Toss payment response JSON. rawJson: {}", rawJson, e);
-            throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR, "결제 응답 처리 중 오류가 발생했습니다.");
-        }
-
-
-        //응답에서 paymentKey 추출
-        String paymentKey = root.path("paymentKey").asText(tossConfirmRequest.paymentKey()); // 파싱실패 대비 폴백
-
-        //저장 및 order.isPaid= true
-        paymentRepository.save(Payment.from(paymentKey, orderId, tossConfirmRequest.amount(), rawJson));
-        orderService.completePayment(order.getId());
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.executeWithoutResult(status -> {
+            //저장 및 order.isPaid= true
+            paymentRepository.save(Payment.from(tossConfirmRequest.paymentKey(), orderId, tossConfirmRequest.amount(), response.toString()));
+            orderService.completePayment(order.getId());
+        });
 
         // 프론트 응답 DTO 만들기
-        return PaymentConfirmResponse.from(root);
+        return PaymentConfirmResponse.from(response);
     }
 }
 
