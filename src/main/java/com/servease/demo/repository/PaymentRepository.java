769b0d59db91137fcedd2097e7bdc99ab80bcddd
wjdwwidz <wjdwwidz@gmail.com>
2025-10-16 18:30:54 +0900

    Feat: Add existsByPaymentKey, existsByExternalOrderId in PaymentRepository

--- src/main/java/com/servease/demo/repository/PaymentRepository.java
+++ src/main/java/com/servease/demo/repository/PaymentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-}
+    boolean existsByPaymentKey(String paymentKey);
 
+    boolean existsByExternalOrderId(String externalOrderId);
+}
