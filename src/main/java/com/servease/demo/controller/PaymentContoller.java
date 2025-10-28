f97880950a76bbf742509c1e8cb5dc249d0b3006
wjdwwidz <wjdwwidz@gmail.com>
2025-10-29 00:01:39 +0900

    Feat: Add payment listing endpoint, paymentDetail

--- src/main/java/com/servease/demo/controller/PaymentContoller.java
+++ src/main/java/com/servease/demo/controller/PaymentContoller.java
@@ -2,10 +2,18 @@ package com.servease.demo.controller;
 
 import com.servease.demo.dto.request.TossConfirmRequest;
 import com.servease.demo.dto.response.PaymentConfirmResponse;
+import com.servease.demo.dto.response.PaymentDetailResponse;
+import com.servease.demo.dto.response.PaymentListResponse;
 import com.servease.demo.service.PaymentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +30,16 @@ public class PaymentContoller {
     public ResponseEntity<PaymentConfirmResponse> confirm(@Valid @RequestBody TossConfirmRequest tossConfirmRequest) {
         return ResponseEntity.ok(paymentService.confirmAndSave(tossConfirmRequest));
     }
+
+    @GetMapping
+    public ResponseEntity<Page<PaymentListResponse>> getPayments(
+            @PageableDefault(sort = "approvedAt", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        return ResponseEntity.ok(paymentService.getPayments(pageable));
+    }
+
+    @GetMapping("/{paymentId}")
+    public ResponseEntity<PaymentDetailResponse> getPaymentDetails(@PathVariable Long paymentId) {
+        return ResponseEntity.ok(paymentService.getPaymentDetail(paymentId));
+    }
 }
-
-
-
