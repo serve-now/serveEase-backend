956a8d645f44da5d54633211a22b1787497d725c
wjdwwidz <wjdwwidz@gmail.com>
2025-10-29 14:15:28 +0900

    Feat: Add ManualSettlementTest

--- src/main/java/com/servease/demo/controller/PaymentContoller.java
+++ src/main/java/com/servease/demo/controller/PaymentContoller.java
@@ -5,6 +5,7 @@ import com.servease.demo.dto.response.PaymentConfirmResponse;
 import com.servease.demo.service.PaymentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/payments")
 public class PaymentContoller {
     private final PaymentService paymentService;
 
     @PostMapping("/confirm")
     public ResponseEntity<PaymentConfirmResponse> confirm(@Valid @RequestBody TossConfirmRequest tossConfirmRequest) {
-        return ResponseEntity.ok(paymentService.confirmAndSave(tossConfirmRequest));
+        log.info("[PAYMENT] confirm request orderId={}, paymentKey={}", tossConfirmRequest.orderId(), tossConfirmRequest.paymentKey());
+        PaymentConfirmResponse response = paymentService.confirmAndSave(tossConfirmRequest);
+        log.info("[PAYMENT] confirm success orderId={}, remainingAmount={}", tossConfirmRequest.orderId(), response.remainingAmount());
+        return ResponseEntity.ok(response);
     }
 }
-
 
 
