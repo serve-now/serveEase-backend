f5c1fdc8d57ba886d7f5b5fb0d27c0e5126a016e
wjdwwidz <wjdwwidz@gmail.com>
2025-11-04 16:35:27 +0900

    Feat: Add specification support for CashPaymentRepository

--- src/main/java/com/servease/demo/repository/CashPaymentRepository.java
+++ src/main/java/com/servease/demo/repository/CashPaymentRepository.java
@@ -2,6 +2,7 @@ package com.servease.demo.repository;
 
 import com.servease.demo.model.entity.CashPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CashPaymentRepository extends JpaRepository<CashPayment, Long> {
+public interface CashPaymentRepository extends JpaRepository<CashPayment, Long>, JpaSpecificationExecutor<CashPayment> {
 }
