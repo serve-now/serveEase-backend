42bf19fc6abd4c9fc388f5e3a89fb377c00e1a05
wjdwwidz <wjdwwidz@gmail.com>
2025-11-10 17:09:29 +0900

    Feat: Add MyPage API for updating password, phone, and store name

--- src/main/java/com/servease/demo/repository/StoreRepository.java
+++ src/main/java/com/servease/demo/repository/StoreRepository.java
@@ -4,6 +4,9 @@ import com.servease.demo.model.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findByIdAndOwnerId(Long storeId, Long ownerId);
 }
